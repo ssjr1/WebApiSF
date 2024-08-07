8fdbead248abbaaeb5ae52306f708e2ee60b7a64
ssjr1 <stalin.jimenezr@ug.edu.ec>
2024-08-07 07:29:28 -0500

    Subida de archivos al repositorio remoto

--- LibreriaSupportFast/obj/Debug/net8.0/LibreriaSupportFast.AssemblyInfo.cs
+++ LibreriaSupportFast/obj/Debug/net8.0/LibreriaSupportFast.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaSupportFast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6223227a6f73275e91e5902dbdb1ee901aac79e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaSupportFast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaSupportFast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
