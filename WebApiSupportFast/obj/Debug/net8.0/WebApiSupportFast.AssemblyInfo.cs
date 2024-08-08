b11f8e051e63316459029e03c086be3cb02640cf
VladoAlvarez <vladimir.alvarezl@ug.edu.ec>
2024-08-08 02:03:39 -0500

    Cambios 0202

--- WebApiSupportFast/obj/Debug/net8.0/WebApiSupportFast.AssemblyInfo.cs
+++ WebApiSupportFast/obj/Debug/net8.0/WebApiSupportFast.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiSupportFast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdbead248abbaaeb5ae52306f708e2ee60b7a64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f47d57ba3229738e10a089e56ee4ced66758583")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiSupportFast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiSupportFast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
