cbfab5f376fff9125626ecdeb9143c5505dd33c1
VladoAlvarez <vladimir.alvarezl@ug.edu.ec>
2024-08-09 18:48:01 -0500

    4to

--- LibreriaSupportFast/obj/Debug/net8.0/LibreriaSupportFast.AssemblyInfo.cs
+++ LibreriaSupportFast/obj/Debug/net8.0/LibreriaSupportFast.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaSupportFast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3836ee57147eef40939022b8cd92ca73e6959218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+309ae62a7c3c9446e95a06e50cc8f3f8aa78dee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaSupportFast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaSupportFast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApiSupportFast/obj/Debug/net8.0/WebApiSupportFast.AssemblyInfo.cs
+++ WebApiSupportFast/obj/Debug/net8.0/WebApiSupportFast.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiSupportFast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3836ee57147eef40939022b8cd92ca73e6959218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+309ae62a7c3c9446e95a06e50cc8f3f8aa78dee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiSupportFast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiSupportFast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
