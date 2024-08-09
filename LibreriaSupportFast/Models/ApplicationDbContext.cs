1b951a99c2096561074a0da66a3a866f633caf6b
VladoAlvarez <vladimir.alvarezl@ug.edu.ec>
2024-08-09 09:36:30 -0500

    Cambios 0936

--- LibreriaSupportFast/Models/ApplicationDbContext.cs
+++ LibreriaSupportFast/Models/ApplicationDbContext.cs
@@ -4,12 +4,12 @@ namespace LibreriaSupportFast.Models
 {
     public class ApplicationDbContext : DbContext
     {
-        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=.;Database=SupportFast;Integrated Security=True;TrustServerCertificate=True;");
-        }
+        }*/
 
         public DbSet<Tecnicos> Tecnicos { get; set; }
         public DbSet<Gestiones> Gestiones { get; set; }
--- WebApiSupportFast/obj/Debug/net8.0/WebApiSupportFast.AssemblyInfo.cs
+++ WebApiSupportFast/obj/Debug/net8.0/WebApiSupportFast.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiSupportFast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aa65f5c90e1a3e2ad3651f26c97b64a4b95325e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ca76026acdccb85d059194c08e70adf5f6a2bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiSupportFast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiSupportFast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
